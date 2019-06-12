7219a0f74c4233722dc166c3d538dd803202f97f
itshulu <1157830249@qq.com>
2019-06-12 19:41:48 +0800

    init

---
+++ tmpp-core/src/main/java/top/sl/tmpp/core/TMPPApplication.java
@@ -0,0 +1,14 @@
+package top.sl.tmpp.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ShuLu
+ */
+@SpringBootApplication
+public class TMPPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TMPPApplication.class, args);
+    }
+}
