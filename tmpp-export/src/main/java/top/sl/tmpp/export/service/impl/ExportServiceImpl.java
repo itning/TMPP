77824d49225532ab1e740086eb363ab92df9ecb1
itshulu <1157830249@qq.com>
2019-06-27 10:10:39 +0800

    studentClassBookTable

--- tmpp-export/src/main/java/top/sl/tmpp/export/service/impl/ExportServiceImpl.java
+++ tmpp-export/src/main/java/top/sl/tmpp/export/service/impl/ExportServiceImpl.java
@@ -14,6 +14,7 @@ import top.sl.tmpp.export.service.ExportService;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -31,37 +32,42 @@ public class ExportServiceImpl implements ExportService {
     @Override
     public void procurementTable(String executePlanId, OutputStream outputStream) throws IOException {
         List<PurchasingMaterials> purchasingMaterials = exportMapper.getPurchasingMaterials(executePlanId);
+
         XSSFWorkbook wb = new XSSFWorkbook();
         XSSFSheet sheet = wb.createSheet("导出采购教材汇总表");
+
         XSSFRow row = sheet.createRow(0);
-        CellRangeAddress region1 = new CellRangeAddress(0, 0, (short) 0, (short) 9);
-        sheet.addMergedRegion(region1);
-        CellStyle style = wb.createCellStyle();
-        XSSFFont font = wb.createFont();
-        font.setFontName("黑体");
-        font.setFontHeightInPoints((short) 16);
-        style.setFont(font);
-        style.setAlignment(HorizontalAlignment.CENTER);
-        XSSFCell cell = row.createCell(0, CellType.STRING);
-        cell.setCellStyle(style);
+        final String[] headerStrArray = {"序号", "教材名称", "出版社", "作者", "书号isbn", "单价", "学生用书", "教师用书", "教务处用书", "购书总数"};
+
+        mergeCells(sheet, headerStrArray.length - 1);
+
+        XSSFCell cell = getCellWithStyle(wb, row);
         cell.setCellValue(exportMapper.selectYear(executePlanId) + "学年第" +
                 ("0".equals(exportMapper.selectTerm(executePlanId)) ? "一" : "二") + "学期采购教材汇总表");
         row = sheet.createRow(1);
-        String[] strs = {"序号", "教材名称", "出版社"
-                , "作者","书号isbn", "单价",  "学生用书",
-                "教师用书", "教务处用书", "购书总数"};
-        information(strs, row, 0);
+        information(headerStrArray, row, 0);
         getSheetByList(purchasingMaterials, sheet);
         wb.write(outputStream);
     }
 
+    /**
+     * 合并单元格
+     *
+     * @param sheet   {@link XSSFSheet}
+     * @param lastCol 最后一格
+     */
+    private void mergeCells(XSSFSheet sheet, int lastCol) {
+        CellRangeAddress region = new CellRangeAddress(0, 0, 0, lastCol);
+        sheet.addMergedRegion(region);
+    }
+
     private <T> void getSheetByList(List<T> list, XSSFSheet sheet) {
         XSSFRow row;
-        for (int r = 2; r < list.size(); r++) {
-            int index = r - 1;
-            row = sheet.createRow(r);
+        for (int r = 0; r < list.size(); r++) {
+            int index = r + 1;
+            row = sheet.createRow(r + 2);
             try {
-                String[] str = getObjectFieldsToArray(list.get(r - 2));
+                String[] str = getObjectFieldsToArray(list.get(r));
                 information(str, row, index);
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
@@ -85,47 +91,92 @@ public class ExportServiceImpl implements ExportService {
 
     @Override
     public void studentClassBookTable(String executePlanId, OutputStream outputStream) throws IOException {
-        List<String> strings = exportMapper.selectClazz(executePlanId);
         XSSFWorkbook wb = new XSSFWorkbook();
-        for (String s : strings) {
-            XSSFSheet sheet = wb.createSheet(s);
-            List<StudentReceiveBook> studentReceiveBooks = exportMapper.selectStudentReceiveBooks(s);
-            studentReceiveBooks.forEach(x->{
-                XSSFRow row = sheet.createRow(0);
-                /*CellRangeAddress region1 = new CellRangeAddress(0, 0, (short) 0, (short) 10);
-                sheet.addMergedRegion(region1);
-                CellStyle style = wb.createCellStyle();
-                style.setAlignment(HorizontalAlignment.CENTER);*/
-                XSSFCell cell = row.createCell(0, CellType.STRING);
-                //cell.setCellStyle(style);
-                cell.setCellValue(exportMapper.selectYear(executePlanId) + "学年第" +
-                        ("0".equals(exportMapper.selectTerm(executePlanId)) ? "一" : "二") + "学期班级领取教材发书单");
-                row = sheet.createRow(1);
-                String[] strs = {"序号", "开课院系", "使用班级"
-                        , "书号isbn", "教材名称", "出版社"
-                        , "单价", "数量","购书总折扣数",  "码洋", "实样"};
-                information(strs, row,0);
-                getSheetByList(studentReceiveBooks, sheet);
-            });
-        }
+        exportMapper.selectClazz(executePlanId)
+                .stream().map(c -> {
+            List<StudentReceiveBook> studentReceiveBooks = exportMapper.selectStudentReceiveBooks(c);
+            return new Tuple<>(c, studentReceiveBooks);
+        }).filter(t -> !t.getT2().isEmpty()).forEach(t -> {
+            XSSFSheet sheet = wb.createSheet(t.getT1());
+            CellRangeAddress region1 = new CellRangeAddress(0, 0, (short) 0, (short) 10);
+            sheet.addMergedRegion(region1);
+
+            XSSFRow row = sheet.createRow(0);
+            final String[] strs = {"序号", "开课院系", "使用班级"
+                    , "书号isbn", "教材名称", "出版社"
+                    , "单价", "数量", "购书总折扣数", "码洋", "实样"};
+            XSSFCell cell = getCellWithStyle(wb, row);
+            cell.setCellValue(exportMapper.selectYear(executePlanId) + "学年第" +
+                    ("0".equals(exportMapper.selectTerm(executePlanId)) ? "一" : "二") + "学期班级领取教材发书单");
+            row = sheet.createRow(1);
+            information(strs, row, 0);
+
+            for (int r = 0; r < t.getT2().size(); r++) {
+                XSSFRow xssfRow = sheet.createRow(r + 2);
+                xssfRow.createCell(0).setCellValue(r + 1);
+
+                StudentReceiveBook book = t.t2.get(r);
+                BigDecimal s = book.getUnitPrice().multiply(BigDecimal.valueOf(book.getClazzNumber()));
+                xssfRow.createCell(1).setCellValue(book.getCollegesName());
+                xssfRow.createCell(2).setCellValue(book.getClazz());
+                xssfRow.createCell(3).setCellValue(book.getIsbn());
+                xssfRow.createCell(4).setCellValue(book.getTextBookName());
+                xssfRow.createCell(5).setCellValue(book.getPress());
+                xssfRow.createCell(6).setCellValue(book.getUnitPrice().toString());
+                xssfRow.createCell(7).setCellValue(book.getClazzNumber());
+                xssfRow.createCell(8).setCellValue(book.getDiscounts().toString());
+                xssfRow.createCell(9).setCellValue(s.doubleValue());
+                xssfRow.createCell(10).setCellValue(s.multiply(book.getDiscounts()).doubleValue());
+            }
+        });
         wb.write(outputStream);
     }
 
+    private XSSFCell getCellWithStyle(XSSFWorkbook wb, XSSFRow row) {
+        CellStyle style = wb.createCellStyle();
+
+        XSSFFont font = wb.createFont();
+        font.setFontHeightInPoints((short) 16);
+        font.setBold(true);
+
+        style.setFont(font);
+        style.setAlignment(HorizontalAlignment.CENTER);
+
+        XSSFCell cell = row.createCell(0, CellType.STRING);
+        cell.setCellStyle(style);
+        return cell;
+    }
+
     private void information(String[] strs, XSSFRow row, int index) {
-        if (index==0){
+        if (index == 0) {
             for (int i = 0; i < strs.length; i++) {
                 row.createCell(i, CellType.STRING).setCellValue(strs[i]);
             }
-        }else {
-            int j=1;
+        } else {
             for (int i = 0; i < strs.length; i++) {
-                if (i==0){
+                if (i == 0) {
                     row.createCell(i, CellType.NUMERIC).setCellValue(index);
                 }
-                row.createCell(i+1, CellType.STRING).setCellValue(strs[i]);
+                row.createCell(i + 1, CellType.STRING).setCellValue(strs[i]);
             }
+        }
+    }
 
+    class Tuple<T1, T2> {
+        private T1 t1;
+        private T2 t2;
+
+        public Tuple(T1 t1, T2 t2) {
+            this.t1 = t1;
+            this.t2 = t2;
         }
 
+        public T1 getT1() {
+            return t1;
+        }
+
+        public T2 getT2() {
+            return t2;
+        }
     }
 }
