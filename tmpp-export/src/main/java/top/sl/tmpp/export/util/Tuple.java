978118c0b3e6fb1d752aa8333c0008ce5b791bb6
itning <itning@itning.top>
2019-06-27 14:06:43 +0800

    fix bug

--- tmpp-export/src/main/java/top/sl/tmpp/export/util/Tuple.java
+++ tmpp-export/src/main/java/top/sl/tmpp/export/util/Tuple.java
@@ -8,7 +8,7 @@ public class Tuple<T1, T2> {
     private T1 t1;
     private T2 t2;
 
-    Tuple(T1 t1, T2 t2) {
+    public Tuple(T1 t1, T2 t2) {
         this.t1 = t1;
         this.t2 = t2;
     }
